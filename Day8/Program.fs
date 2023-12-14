cca5c384fdfaa7a9afaf0b367c3f1fa39e22dc3a
naomiEve <0xlunaric@gmail.com>
2023-12-14 17:12:27 +0100

    feat: day8 pt1 & day9
    kinda got lazy in the meantime orz

---
+++ Day8/Program.fs
@@ -0,0 +1,83 @@
+﻿open System.Collections.Generic
+open System.IO
+
+type Node = {
+    Name: string
+    Children: string array
+}
+
+let parseNode (line: string) =
+    let parts = line.Split(" = ")
+    let nodeName = parts[0]
+    let children = parts[parts.Length - 1]
+                       .Replace("(", "")
+                       .Replace(")", "")
+                       .Split(", ")
+    {
+        Name = nodeName;
+        Children = children
+    }
+    
+let lines = File.ReadAllLines("data.in")
+
+let parseNodes =
+    lines |>
+    Seq.skip 2 |>
+    Seq.map parseNode |>
+    Seq.map (fun node ->
+        (node.Name, node)) |>
+    Map.ofSeq
+    
+let nthNode n =
+    lines |>
+    Seq.skip (2 + n) |>
+    Seq.pick (fun line ->
+        Some(parseNode line))
+    
+let getNodeByName nodes name =
+    Map.find name nodes
+    
+let traverseUntil directions nodes nodeName start =
+    let mutable loop = []
+    let rec recursiveTraversal (directions: string) nodes destNode currentNode i =
+        let point = currentNode.Name + ((i % directions.Length) |> string)
+        match (Seq.contains point loop) with
+        | true ->
+            None
+        | false ->
+            loop <- point :: loop
+            match currentNode.Name = destNode with
+            | true -> Some(i)
+            | false ->
+                let nextDirection = directions[i % directions.Length]
+                match nextDirection with
+                | 'R' ->
+                    recursiveTraversal directions nodes destNode (getNodeByName nodes currentNode.Children[1]) (i + 1)
+                | 'L' ->
+                    recursiveTraversal directions nodes destNode (getNodeByName nodes currentNode.Children[0]) (i + 1)
+                | _ -> None
+        
+    recursiveTraversal directions nodes nodeName start 0
+    
+let part1 =
+    let directions = lines |>
+                     Seq.head
+                     
+    let nodes = parseNodes
+
+    match traverseUntil directions nodes "ZZZ" (getNodeByName nodes "AAA") with
+    | None -> -1
+    | Some value -> value
+    
+let part2 =
+    let directions = lines |>
+                     Seq.head
+                     
+    let nodes = parseNodes
+    nodes |>
+    Map.toSeq |>
+    Seq.filter (fun (name, _) ->
+        name.EndsWith('A')) |>
+    ignore
+    
+printfn $"Part one: %d{part1}"
---
+++ Day9/Program.fs
@@ -0,0 +1,66 @@
+﻿open System.IO
+
+let createDiffTable seq =
+    seq |>
+    Seq.skip 1 |>
+    Seq.fold (fun (list, i) item ->
+        let prev = Seq.item (i - 1) seq
+        ((item - prev) :: list, i + 1)
+        ) ([], 1) |>
+    fst |>
+    List.rev
+    
+let lineToIntSeq (line: string) =
+    line.Split [|' '|] |>
+    Seq.map int |>
+    List.ofSeq
+    
+let processUntilSequenceAreZeroes seq =
+    let rec processSeq list seq =
+        let diffTbl = createDiffTable seq
+        let allZeroes = Seq.forall (fun a -> a = 0) diffTbl
+        match allZeroes with
+        | true -> list
+        | false ->
+            processSeq (diffTbl :: list) diffTbl
+            
+    processSeq [List.ofSeq seq] seq
+    
+let predictNext seq =
+    seq |>
+    Seq.fold (fun acc list ->
+        let last = list |>
+                   Seq.last
+        
+        last + acc
+        ) 0
+    
+let predictPrev seq =
+    seq |>
+    Seq.fold (fun acc list ->
+        let last = list |>
+                   Seq.head
+        
+        last - acc
+        ) 0
+    
+let lines = File.ReadAllLines "data.in"
+
+let part1 =
+    lines |>
+    Seq.map (fun line ->
+        lineToIntSeq line |>
+        processUntilSequenceAreZeroes |>
+        predictNext) |>
+    Seq.sum
+    
+let part2 =
+    lines |>
+    Seq.map (fun line ->
+        lineToIntSeq line |>
+        processUntilSequenceAreZeroes |>
+        predictPrev) |>
+    Seq.sum
+    
+printfn $"Part one: %d{part1}"
+printfn $"Part one: %d{part2}"
